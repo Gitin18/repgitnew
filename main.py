29fbac0c83938a5f101db4f052d71863e3183680
Gitin18 <inka25618@gmail.com>
2024-10-14 15:06:45 +0200

    remove and change

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 import math
 
-def are_coprime(a, b):
-    return math.gcd(a, b) == 1
+def gcd_three_numbers(a, b, c):
+    return math.gcd(math.gcd(a, b),c) 
 
-# Request two integers from the user
-num1 = int(input("Enter the first integer: "))
-num2 = int(input("Enter the second integer: "))
-
-# Check if the numbers are coprime56
-gcd_value = math.gcd(num1, num2)
-if are_coprime(num1, num2):
-    print(f"{num1} and {num2} are coprime.")
-else:
-    print(f"{num1} and {num2} are not coprime. Their greatest common divisor is {gcd_value}.")
 
 
