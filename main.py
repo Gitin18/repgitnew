1b59a3ad0b20b1db590eab2f19e68458df3c404d
Gitin18 <inka25618@gmail.com>
2024-10-14 11:13:56 +0200

    changes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def are_coprime(a, b):
 num1 = int(input("Enter the first integer: "))
 num2 = int(input("Enter the second integer: "))
 
-# Check if the numbers are coprime
+# Check if the numbers are coprime56
 gcd_value = math.gcd(num1, num2)
 if are_coprime(num1, num2):
     print(f"{num1} and {num2} are coprime.")
